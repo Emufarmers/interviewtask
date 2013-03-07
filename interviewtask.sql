d4c1515334f290b673a066ee09abc4d5e5e039d6
Benjamin Lees <emufarmers@gmail.com>
2013-03-07 03:28:36 -0500

    hmm, probably don't need that :)

--- interviewtask.sql
+++ interviewtask.sql
@@ -1,6 +1,5 @@
-CREATE TABLE /*_*/currency_conversions (
+CREATE TABLE currency_conversions (
   currency_code varchar(3) NOT NULL PRIMARY KEY,
 
-  -- Number of times the question has been correctly answered
   exchange_rate float NOT NULL
-) /*$wgDBTableOptions*/;
+)
